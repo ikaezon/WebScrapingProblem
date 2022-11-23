8fb374d258ec3ca99f55cb068236ba3d50c02a51
Kaevon Zanjani <77306454+ikaezon@users.noreply.github.com>
2022-11-22 19:30:27 -0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+import os
+import requests
+from urllib.parse import urljoin
+from bs4 import BeautifulSoup
+
+#URL scraping from
+url = "https://cms.math.ca/competitions/cmo/"
+
+#Define location, if there is no such folder, script will create one automatically
+folder_location = r'E:\MathProblems'
+if not os.path.exists(folder_location):os.mkdir(folder_location)
+
+#Parse web page for files ending in .PDF
+response = requests.get(url)
+soup= BeautifulSoup(response.text, "html.parser")
+for link in soup.select("a[href$='.pdf']"):
+    #Name the pdf files using the last portion of each link which are unique in this case
+    filename = os.path.join(folder_location,link['href'].split('/')[-1])
+    with open(filename, 'wb') as f:
+        f.write(requests.get(urljoin(url,link['href'])).content)
